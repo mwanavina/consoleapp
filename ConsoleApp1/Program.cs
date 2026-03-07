1ac22f5e7b1546cfae202a5659c74118282cb262
mwanavina <mmwanavina@gmail.com>
2026-03-07 14:41:30 -0800

    Add generated files for .NET 10.0 project setup
    - Created project.assets.json to manage package dependencies.
    - Added ConsoleApp1.csproj.nuget.g.props and ConsoleApp1.csproj.nuget.g.targets for NuGet integration.
    - Generated assembly attributes and info files for ConsoleApp1.
    - Included global usings and editor config for project settings.
    - Added necessary DLLs, PDBs, and runtime configuration files for .NET 10.0.
    - Updated project.nuget.cache for package management.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// Console.WriteLine("Hello World!");
+Console.Write("Congratulations!");
+Console.Write("");
+Console.Write("Yeah");
