fef4dd61d544c7c2dcf239ba885bc16de902cdbd
mwanavina <mmwanavina@gmail.com>
2026-03-08 14:25:29 -0800

    Update assembly information and binary files for ConsoleApp1

--- ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d29d6c3a7ad9a0fea22ecc38b8bc153efec6a2ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+250495a33992aa436c83bf3a4dfb38744cb5e14f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
